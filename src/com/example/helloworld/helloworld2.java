05a5399e43e0bf96371f609b3217151b0c04d7f3
dharshana-08 <dharshana2808@gmail.com>
2025-12-14 13:24:33 +0530

    Here i modify

--- src/com/example/helloworld/helloworld2.java
+++ src/com/example/helloworld/helloworld2.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 public class helloworld2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("Dog is barking");
 	}
 
 }
